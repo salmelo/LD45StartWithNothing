3d67941017b9d5efaa4e50dc6a5bde40919ccc61
Salvador Melo <salmelo16@gmail.com>
2019-10-05 22:49:35 -0400

    empty room

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public GameObject testRoom;
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = GetComponent<Grid>();
+
+        for (int x = -5; x < 5; x++)
+        {
+            for (int y = -5; y < 5; y++)
+            {
+                Instantiate(testRoom, grid.CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity, transform);
+            }
+        }
+    }
+}
